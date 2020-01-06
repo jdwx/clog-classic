6901be4cf3df4c64c6495eabeae3c448316c5c55
JDW <jdw@slab.home.wheelhouse.org>
2020-01-05 19:07:54 -0500

    Code cleanup.

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Background
 
-This patch grew out of a project needing to support syslog files on 
-an embedded platform machine with only RAM and flash.  No disk and no 
-swap space.  Placing the log files on the flash disk would introduce 
-excessive wear on the flash device.  Placing the log files in RAM 
+This patch grew out of a project needing to support syslog files on
+an embedded platform machine with only RAM and flash.  No disk and no
+swap space.  Placing the log files on the flash disk would introduce
+excessive wear on the flash device.  Placing the log files in RAM
 resulted in a slow squeeze of available memory as they grew ever larger.
 
 By introducing circular logfiles, it becomes possible to put the syslog files
@@ -13,7 +13,7 @@ on a RAM disk but contain them at a constant size that can be planned for
 and does not require routine maintenance inappropriate to an embedded device.
 
 Support for circular logfiles requires a small patch to syslogd and a support
-program called clog.  The clog program creates the circular logfiles and can 
+program called clog.  The clog program creates the circular logfiles and can
 be used to view their contents.
 
 ## Downloads
@@ -22,11 +22,11 @@ Source code for this software is available under the BSD public license.
 
 In order to build the patched syslogd, the clog source must be available in
 a sibling directory.  That is, if syslogd is in /usr/src/usr.sbin/syslogd,
-then clog should be unpacked to /usr/src/usr.sbin/clog.  
+then clog should be unpacked to /usr/src/usr.sbin/clog.
 
 ## Release History
 
-* 1.0.1: Fixed an oversight in clog that made it act as if the -f 
+* 1.0.1: Fixed an oversight in clog that made it act as if the -f
 option was always in use. 10/2/2001
 
 * 1.0: Initial release. 10/1/2001
--- clog/clog.c
+++ clog/clog.c
@@ -1,31 +1,3 @@
-/*-
- * Copyright (c) 2001
- *     Jeff Wheelhouse (jdw@wwwi.com)
- *
- * This code was originally developed by Jeff Wheelhouse (jdw@wwwi.com).
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistribution of source code must retail the above copyright 
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY JEFF WHEELHOUSE ``AS IS'' AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN 
- * NO EVENT SHALL JEFF WHEELHOUSE BE LIABLE FOR ANY DIRECT, INDIRECT, 
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING BUT NOT 
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *  $Id: clog.c,v 1.3 2001/10/02 18:51:26 jdw Exp $
- */
 
 
 #include <assert.h>
@@ -48,7 +20,7 @@
 
 
 /*
- *  The BUFFER_SIZE value is just used to allocate a buffer full of NULLs 
+ *  The BUFFER_SIZE value is just used to allocate a buffer full of NULLs
  *  so that a new logfile can be extended to its full size.
  *
  *  Compiling with -pedantic complains when the buffer array is declared
@@ -186,7 +158,7 @@ void init_log(const char *lname, size_t size) {
 
 	(void)memset(buffer,0,BUFFER_SIZE);
 
-	fd = open(lname,O_RDWR|O_CREAT,0666); 
+	fd = open(lname,O_RDWR|O_CREAT,0666);
 	if (fd==-1) {
 		fprintf(stderr,"%s: ERROR: could not open %s (%s)\n",pname,lname,strerror(errno));
 		exit(2);
